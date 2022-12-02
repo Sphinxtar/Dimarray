54d6b29718386741df4fc72faa79da3b48eea4b3
Sphinxtar <sphinxtar@gmail.com>
2022-12-02 13:06:17 -0700

    first commit
    	modified:   README.md
    	new file:   dimarray.c
    	new file:   Makefile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Dimarray
+<<<<<<< HEAD
 Dimension and allocate arrays at runtime in C
+=======
+dimension and allocate array at runtime in C
+
+>>>>>>> 8fadce7 (first commit)
---
+++ dimarray.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+// prototype
+
+double **dimarray( int x, int y )
+{
+	double **ray = NULL;
+	ray = (double **)malloc( y * sizeof(double *));
+	*ray = (double *)malloc( x * y * sizeof(double));
+	for( int i = 1; i < y; ++i ){
+		*(ray + i) = *(ray + i - 1) + x;
+	}
+	return(ray);
+}
+
+int  main( void )
+{
+	int retval = 0;
+	double **ray = dimarray(6, 6);
+	ray[3][3] = 5.0;
+	ray[5][5] = 12345.123;
+	printf("%f\n", ray[3][3]+ray[5][5]);
+	free(ray);
+	return(retval);
+}
+//eof
